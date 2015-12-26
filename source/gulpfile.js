51e6356521513214ffe9ce9dcd52fbe470ec101c
刘强 <thinkgarden1990@163.com>
2015-12-26 22:06:09 +0800

    browsersync

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -5,24 +5,39 @@ var prefix = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
 
 function streamError(err) {
   gutil.beep();
   gutil.log(err instanceof gutil.PluginError ? err.toString() : err.stack);
 }
 
+gulp.task('browser-sync', ['sass', 'build'], function () {
+  browserSync({
+    server:{
+      baseDir: '..'
+    }
+  });
+})
+
 gulp.task('build', function () {
   gulp.src('pages/*.html')
       .pipe(wrap({src:'layout/default.html'}))
       .pipe(gulp.dest('..'));
-})
+});
+
+gulp.task('rebuild', ['build'], function () {
+    browserSync.reload();
+});
+
 
 gulp.task('sass',function(){
-    gulp.src('styles/main.scss')
+    gulp.src('styles/**/*.scss')
         .pipe(plumber({errorHandler: streamError}))
         .pipe(sass())
         .pipe(prefix())
-        .pipe(gulp.dest('../styles'));
+        .pipe(gulp.dest('../styles'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('watch', function () {
@@ -30,4 +45,4 @@ gulp.task('watch', function () {
   gulp.watch(['styles/*.scss'], ['sass']);
 })
 
-gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
