befb75c952351d052e678ab74457a1d9d437447a
刘强 <thinkgarden1990@163.com>
2015-12-26 21:08:01 +0800

    font-awesome

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+
+function streamError(err) {
+  gutil.beep();
+  gutil.log(err instanceof gutil.PluginError ? err.toString() : err.stack);
+}
 
 gulp.task('sass',function(){
     gulp.src('styles/main.scss')
+        .pipe(plumber({errorHandler: streamError}))
         .pipe(sass())
         .pipe(prefix())
         .pipe(gulp.dest('../styles'));
